804f9fce2b0c00bf53e46630936d28dcc8d4c09a
AlaIchhe <zhuhe06@126.com>
2026-07-31 23:16:25 +0800

    feat: 更新版本号至 1.2.1

--- src/hdu_sniper/__init__.py
+++ src/hdu_sniper/__init__.py
@@ -1,6 +1,6 @@
 """HDU Library Sniper 应用包。"""
 
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 
 from hdu_sniper.app import SniperApp
 from hdu_sniper.events import ApplicationEvent, EventKind, JobState
